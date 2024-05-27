0ffee2cfd4d2d910e0b6081a846e228e1b2eb746
Alex Schirokow <aschirokow@gmx.de>
2024-05-27 10:35:01 +0200

    prep models - code corrections

--- models/prep/prep_daily.sql
+++ models/prep/prep_daily.sql
@@ -8,20 +8,20 @@ add_features AS (
 		, DATE_PART('month', date) AS date_month
 		, DATE_PART('year', date) AS date_year
 		, DATE_PART('week', date) AS cw
-		, TO_CHAR(date, 'FMmonth') AS monthname
+		, TO_CHAR(date, 'FMmonth') AS month_name
 		, TO_CHAR(date, 'FMday') AS weekday
-    FROM staging_daily
-    ORDER BY date
+    FROM daily_data 
 ),
 add_more_features AS (
     SELECT *
 		, (CASE 
-			WHEN monthname in ('december','january','february') THEN 'winter'
-			WHEN monthname in ('march','april','may') THEN 'spring'
-            WHEN monthname in ('june','july','august') THEN 'summer'
-            WHEN monthname in ('september','october','november') THEN 'autumn'
+			WHEN month_name in ('december','january','february') THEN 'winter'
+			WHEN month_name in ('march','april','may') THEN 'spring'
+            WHEN month_name in ('june','july','august') THEN 'summer'
+            WHEN month_name in ('september','october','november') THEN 'autumn'
 		END) AS season
-		FROM add_features
+    FROM add_features
 )
 SELECT *
 FROM add_more_features
+ORDER BY date
